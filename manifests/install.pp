8bb6186ddd5cfbeff3e1f6c270e69df8e9dff033
@slovacus <slovacus@gmail.com>
2015-07-05 16:43:40 -0500

    [feature/#2] fixed variables and install package

--- manifests/install.pp
+++ manifests/install.pp
@@ -4,4 +4,36 @@
 #
 class nodejs::install {
 
+  exec{"nodejs clone repository":
+    command => "git clone ${::nodejs::repository} ${::nodejs::app_name} ",
+    cwd => $::nodejs::path_tmp,
+    require => [Package['git']],
+    unless => "ls -l ${::nodejs::full_path}"
+  }
+
+  exec{"nodejs configure":
+    command => "configure --openssl-libpath=/usr/lib/ssl"
+    cwd => $::nodejs::full_path,
+    unless => "which ${::nodejs::app_name}",
+    require => [Exec["nodejs clone repository"],
+                Package[$::nodejs::package_require]],
+    logoutput=> true,
+  }
+
+  exec{"nodejs make":
+    command => "make",
+    cwd => $::nodejs::full_path,
+    unless => "which ${::nodejs::app_name}",
+    require => [Exec["nodejs configure"]],
+    logoutput=> true,
+  }
+
+  exec{"nodejs make install":
+    command => "make install",
+    cwd => $::nodejs::full_path,
+    unless => "which ${::nodejs::app_name}",
+    require => [Exec["nodejs configure"]],
+    logoutput=> true,
+  }
+
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -7,13 +7,18 @@ class nodejs::params {
 
   $repository = "git@github.com:joyent/node.git"
   $path_tmp = "/var/tmp/"
+  $app_name = "node"
+  $full_path = $path_tmp . $app_name
 
   case $::osfamily {
     'Debian': {
       $package_require = [
                           "git",
+                          "curl",
+                          "build-essential",
+                          "openssl",
+                          "libssl-dev",
                             ]
-      $app_name = "nodejs"
       $version_repository = "master"
     }
     'RedHat', 'Amazon': {
